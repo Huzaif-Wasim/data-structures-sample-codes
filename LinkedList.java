5e91c83f601286e5d4b970a9db748515ec167016
Huzaif <huzaifwasim.nitrr@gmail.com>
2019-07-09 14:44:13 +0530

    doubly linked list

---
+++ LinkedList.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mdhqureshi
+ */
+public class LinkedList<T> {
+    
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+    
+    public LinkedList(){
+        size=0;
+        head=tail=null;
+    }
+    private class Node<T>{
+        T data;
+        Node<T> next;
+        Node<T> prev;
+    
+        public Node(T data){
+            this.data=data;
+            Node<T> next=null;
+            Node<T> prev = null;
+        } 
+    }
+    
+    public int size(){
+        return this.size;
+    }
+    
+    public boolean isEmpty(){
+        return (this.size == 0);
+    }
+    
+    public void append(T data){
+        Node<T> temp = new Node(data);
+        if (this.isEmpty()){
+        head=tail=temp;
+        ++size;
+        }
+        else{
+            temp.next=null;
+            temp.prev=tail;
+            tail.next=temp;
+            tail=temp;
+            ++size;
+        }
+        System.out.println("Appended");
+    }
+    
+    public void addHead(T data){
+        Node<T> temp = new Node<T>(data);
+        if(this.isEmpty()){
+            head=tail=temp;
+            ++size;
+        }
+        else{
+        temp.prev=null;
+        temp.next=head;
+        head=temp;
+        ++size;
+        }
+    }
+    
+    public void printList(){
+        if(this.isEmpty()){
+        System.out.println("Empty list");
+        }
+        else{
+            Node<T> trav = head;
+            while(trav != null){
+            System.out.println(trav.data);
+            trav=trav.next;
+            }
+        }
+    }
+    
+}
