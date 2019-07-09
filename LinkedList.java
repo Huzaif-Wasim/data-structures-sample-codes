ecf24958fdd28c86f45be237ae9b2c8cf2572614
Huzaif <huzaifwasim.nitrr@gmail.com>
2019-07-09 16:32:12 +0530

    doubly linked list insertion/deletion added

--- LinkedList.java
+++ LinkedList.java
@@ -9,76 +9,113 @@
  * @author mdhqureshi
  */
 public class LinkedList<T> {
-    
+
     private int size;
-    private Node<T> head;
+    public Node<T> head;
     private Node<T> tail;
-    
-    public LinkedList(){
-        size=0;
-        head=tail=null;
+
+    public LinkedList() {
+        size = 0;
+        head = tail = null;
     }
-    private class Node<T>{
+
+    public class Node<T> {
         T data;
         Node<T> next;
         Node<T> prev;
-    
-        public Node(T data){
-            this.data=data;
-            Node<T> next=null;
+
+        public Node(T data) {
+            this.data = data;
+            Node<T> next = null;
             Node<T> prev = null;
-        } 
+        }
     }
-    
-    public int size(){
+
+    public int size() {
         return this.size;
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return (this.size == 0);
     }
-    
-    public void append(T data){
-        Node<T> temp = new Node(data);
-        if (this.isEmpty()){
-        head=tail=temp;
-        ++size;
-        }
-        else{
-            temp.next=null;
-            temp.prev=tail;
-            tail.next=temp;
-            tail=temp;
+
+    public void append(T data) {
+        Node<T> temp = new Node<T>(data);
+        if (this.isEmpty()) {
+            head = tail = temp;
+            ++size;
+        } else {
+            temp.next = null;
+            temp.prev = tail;
+            tail.next = temp;
+            tail = temp;
             ++size;
         }
         System.out.println("Appended");
     }
-    
-    public void addHead(T data){
+
+    public void addHead(T data) {
         Node<T> temp = new Node<T>(data);
-        if(this.isEmpty()){
-            head=tail=temp;
+        if (this.isEmpty()) {
+            head = tail = temp;
+            ++size;
+        } else {
+            temp.prev = null;
+            temp.next = head;
+            head = temp;
             ++size;
         }
-        else{
-        temp.prev=null;
-        temp.next=head;
-        head=temp;
-        ++size;
+    }
+
+    public void insertAfter(Node<T> ref, T data) {
+        Node<T> temp = new Node<T>(data);
+        if (ref == null) {
+            throw new IllegalArgumentException("Null reference entered");
+        } else {
+            temp.next = ref.next;
+            temp.prev = ref;
+            ref.next = temp;
+            temp.next.prev = temp;
+            size++;
         }
     }
-    
-    public void printList(){
-        if(this.isEmpty()){
-        System.out.println("Empty list");
-        }
-        else{
+
+    public void deleteByData(T data) {
+        if (isEmpty()) {
+            throw new RuntimeException("Linked List is already empty");
+        } else {
             Node<T> trav = head;
-            while(trav != null){
-            System.out.println(trav.data);
-            trav=trav.next;
+            while (trav != null) {
+                if (trav.data == data) {
+
+                    if (trav.prev == null) {
+                        head = trav.next;
+                        head.prev = null;
+                    } else if (trav.next == null) {
+                        tail = trav.prev;
+                        tail.next = null;
+                    } else {
+                        trav.prev.next = trav.next;
+                        trav.next.prev = trav.prev;
+                    }
+                    size--;
+                    break;
+                }
+                trav = trav.next;
             }
         }
     }
-    
+
+    public void printList() {
+        if (this.isEmpty()) {
+            System.out.println("Empty list");
+        } else {
+            Node<T> trav = head;
+            while (trav != null) {
+                System.out.println(trav.data);
+                trav = trav.next;
+            }
+        }
+    }
+
 }
